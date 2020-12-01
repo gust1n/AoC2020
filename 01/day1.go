9204a8b335cfe4eb1781b8492e91a1caf0b887d9
Joakim Gustin <joakim.gustin@evolvetechnology.se>
2020-12-01 08:30:05 +0100

    Remove unneccessary check for self

--- 01/day1.go
+++ 01/day1.go
@@ -7,9 +7,6 @@ LOOP:
 	for _, n1 := range input {
 		// try with each other inpt
 		for _, n2 := range input {
-			if n1 == n2 { // skip self
-				continue
-			}
 			if n1+n2 == 2020 {
 				fmt.Printf("found %d and %d, with the product %d\n", n1, n2, n1*n2)
 				break LOOP
