c3554681311085ea517a531cd39b32066a917cec
Joakim Gustin <joakim.gustin@evolvetechnology.se>
2020-12-04 20:19:51 +0100

    Cleanup 04

--- 04/main.go
+++ 04/main.go
@@ -9,34 +9,38 @@ import (
 	"strings"
 )
 
+type passport map[string]string
+
 func main() {
 	lines := readStringsFromFile("./input.txt")
 	requiredFields := []string{"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"}
 
+	var entryStartIdx int
+	var passports [][]string
+
+	for i, line := range lines {
+		if len(line) == 0 { // new passport
+			var passport []string
+			for j := entryStartIdx; j < i; j++ { // each line for this passport
+				passport = append(passport, strings.Fields(lines[j])...)
+			}
+			entryStartIdx = i + 1
+			passports = append(passports, passport)
+		}
+		if i+1 == len(lines) { // last line
+			var passport []string
+			for j := entryStartIdx; j < len(lines); j++ { // each line for this passport
+				passport = append(passport, strings.Fields(lines[j])...)
+			}
+			passports = append(passports, passport)
+		}
+	}
+
 	func() {
 		fmt.Println("----part 1----")
 
-		var entryStartIdx int
 		var numValid int
-		var passports [][]string
 
-		for i, line := range lines {
-			if len(line) == 0 { // new passport
-				var passport []string
-				for j := entryStartIdx; j < i; j++ { // each line for this passport
-					passport = append(passport, strings.Fields(lines[j])...)
-				}
-				entryStartIdx = i + 1
-				passports = append(passports, passport)
-			}
-			if i+1 == len(lines) { // last line
-				var passport []string
-				for j := entryStartIdx; j < len(lines); j++ { // each line for this passport
-					passport = append(passport, strings.Fields(lines[j])...)
-				}
-				passports = append(passports, passport)
-			}
-		}
 		for _, passport := range passports {
 			if hasRequiredFields(passport, requiredFields) {
 				numValid++
@@ -49,26 +53,7 @@ func main() {
 	func() {
 		fmt.Println("----part 2----")
 
-		var entryStartIdx int
 		var numValid int
-		var passports [][]string
-		for i, line := range lines {
-			if len(line) == 0 { // new passport
-				var passport []string
-				for j := entryStartIdx; j < i; j++ { // each line for this passport
-					passport = append(passport, strings.Fields(lines[j])...)
-				}
-				entryStartIdx = i + 1
-				passports = append(passports, passport)
-			}
-			if i+1 == len(lines) { // last line
-				var passport []string
-				for j := entryStartIdx; j < len(lines); j++ { // each line for this passport
-					passport = append(passport, strings.Fields(lines[j])...)
-				}
-				passports = append(passports, passport)
-			}
-		}
 
 		for _, passport := range passports {
 			if hasRequiredFields(passport, requiredFields) && hasValidValues(passport) {
@@ -97,21 +82,21 @@ func hasRequiredFields(passport []string, requiredFields []string) bool {
 
 }
 
-// rules:
-
-// byr (Birth Year) - four digits; at least 1920 and at most 2002.
-// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
-// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
-// hgt (Height) - a number followed by either cm or in:
-//     If cm, the number must be at least 150 and at most 193.
-//     If in, the number must be at least 59 and at most 76.
-// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
-// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
-// pid (Passport ID) - a nine-digit number, including leading zeroes.
-// cid (Country ID) - ignored, missing or not.
-
 var hclReg = regexp.MustCompile(`#[a-f0-9]{6}`)
 var pidReg = regexp.MustCompile(`[0-9]{9}`)
+var yearRegexp = regexp.MustCompile(`^[0-9]{4}$`)
+
+func yearValid(s string, min, max int) bool {
+	if !yearRegexp.MatchString(s) {
+		return false
+	}
+	i := parseInt(s)
+	if i < min || i > max {
+		return false
+	}
+
+	return true
+}
 
 func hasValidValues(passport []string) bool {
 	keys := map[string]string{}
@@ -120,34 +105,32 @@ func hasValidValues(passport []string) bool {
 		keys[kv[0]] = kv[1]
 	}
 
-	// byr
-	byr := parseInt(keys["byr"])
-	if byr < 1920 || byr > 2002 {
+	// byr (Birth Year) - four digits; at least 1920 and at most 2002.
+	if !yearValid(keys["byr"], 1920, 2002) {
 		return false
 	}
 
-	// iyr
-	iyr := parseInt(keys["iyr"])
-	if iyr < 2010 || iyr > 2020 {
+	// iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+	if !yearValid(keys["iyr"], 2010, 2020) {
 		return false
 	}
 
-	// eyr
-	eyr := parseInt(keys["eyr"])
-	if eyr < 2020 || eyr > 2030 {
+	// eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+	if !yearValid(keys["eyr"], 2020, 2030) {
 		return false
 	}
 
-	// hgt
+	// hgt (Height) - a number followed by either cm or in:
+	//     If cm, the number must be at least 150 and at most 193.
+	//     If in, the number must be at least 59 and at most 76.
 	hgt := keys["hgt"]
-	unit := hgt[max(0, len(hgt)-2):len(hgt)]
-	if unit == "cm" {
-		value := parseInt(hgt[0 : len(hgt)-2])
+	if strings.HasSuffix(hgt, "cm") {
+		value := parseInt(strings.TrimSuffix(hgt, "cm"))
 		if value < 150 || value > 193 {
 			return false
 		}
-	} else if unit == "in" {
-		value := parseInt(hgt[0 : len(hgt)-2])
+	} else if strings.HasSuffix(hgt, "in") {
+		value := parseInt(strings.TrimSuffix(hgt, "in"))
 		if value < 59 || value > 76 {
 			return false
 		}
@@ -155,21 +138,21 @@ func hasValidValues(passport []string) bool {
 		return false
 	}
 
-	// hcl
+	// hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
 	hcl := keys["hcl"]
-	if !hclReg.Match([]byte(hcl)) {
+	if !hclReg.MatchString(hcl) {
 		return false
 	}
 
-	// ecl
+	// ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
 	ecl := keys["ecl"]
 	if len(ecl) != 3 || !strings.Contains("amb blu brn gry grn hzl oth", ecl) {
 		return false
 	}
 
-	// pid
+	// pid (Passport ID) - a nine-digit number, including leading zeroes.
 	pid := keys["pid"]
-	if !pidReg.Match([]byte(pid)) {
+	if !pidReg.MatchString(pid) {
 		return false
 	}
 
